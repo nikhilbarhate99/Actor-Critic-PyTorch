be015f1b8616615e0a7f31c8b6791a0bf0b48161
Nikhil Barhate <nikhilbarhate99@gmail.com>
2018-09-03 20:24:00 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,14 +5,15 @@ import torch.optim as optim
 import gym
 
 def train():
+    # Hyperparameters : 
     env = gym.make('LunarLander-v2')
     render = True
     gamma = 0.99
-
     policy = ActorCritic()
-    optimizer = optim.Adam(policy.parameters(), lr=3e-2, betas=(0.8, 0.999))
-    running_reward = 0
+    optimizer = optim.Adam(policy.parameters(), lr=0.03, betas=(0.81, 0.999))
+    
     for i_episode in range(1, 10000):
+        running_reward = 0
         state = env.reset()
         for t in range(10000):
             action = policy(state)
@@ -21,19 +22,23 @@ def train():
             running_reward += reward
             if render and i_episode > 1000:
                 env.render()
-                torch.save(policy.state_dict(), './preTrained/LunarLander.pth')
             if done:
                 break
+        
+        # Updating the policy :
         optimizer.zero_grad()
         loss = policy.calculateLoss(gamma)
         loss.backward()
         optimizer.step()
         policy.clearMemory()
         
+        if i_episode > 1000:
+            torch.save(policy.state_dict(), './preTrained/LunarLander.pth')
+            
         if i_episode % 10 == 0:
-            print('Episode {}\tLast length: {:5d}'.format(
+            print('Episode {}\tlength: {}'.format(
                 i_episode, t))
-        if running_reward > env.spec.reward_threshold:
+        if running_reward > 1000:
             print("Solved!")
             test()
             break
