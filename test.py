3d41c40c7a77529ae5e1bcb7513df5249020cde2
Nikhil Barhate <nikhilbarhate99@gmail.com>
2018-09-03 18:20:29 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,27 @@
+from model import ActorCritic
+import torch
+import gym
+from PIL import Image
+def test():
+    env = gym.make('LunarLander-v2')
+    policy = ActorCritic()
+    policy.load_state_dict(torch.load('./preTrained/LunarLander TWO.pth'))
+    render = True
+    save_gif = True
+
+    for i_episode in range(1, 10):
+        state = env.reset()
+        for t in range(10000):
+            action = policy(state)
+            state, reward, done, _ = env.step(action)
+            if render:
+                 env.render()
+                 if save_gif:
+                     img = env.render(mode = 'rgb_array')
+                     img = Image.fromarray(img)
+                     img.save('./gif/{}.png'.format(t))
+            if done:
+                break
+            
+if __name__ == '__main__':
+    test()
