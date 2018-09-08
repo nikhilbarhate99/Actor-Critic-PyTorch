ff81fcb1fa465e6eccf8066f3b1c641d3f18834b
Nikhil Barhate <nikhilbarhate99@gmail.com>
2018-09-08 17:54:15 +0530

    Update model.py

--- model.py
+++ model.py
@@ -8,8 +8,8 @@ class ActorCritic(nn.Module):
         super(ActorCritic, self).__init__()
         self.affine = nn.Linear(8, 128)
         
-        self.value_layer = nn.Linear(128, 1)
         self.action_layer = nn.Linear(128, 4)
+        self.value_layer = nn.Linear(128, 1)
         
         self.logprobs = []
         self.state_values = []
@@ -32,22 +32,20 @@ class ActorCritic(nn.Module):
     
     def calculateLoss(self, gamma=0.99):
         
-        # Calculating Discounted Rewards :
-        discounted_rewards = []
+        # calculating discounted rewards:
+        rewards = []
         dis_reward = 0
         for reward in self.rewards[::-1]:
             dis_reward = reward + gamma * dis_reward
-            discounted_rewards.insert(0, dis_reward)
+            rewards.insert(0, dis_reward)
+                
+        # normalizing the rewards:
+        rewards = torch.tensor(rewards)
+        rewards = (rewards - rewards.mean()) / (rewards.std())
         
-        # Normalizing the Rewards :
-        discounted_rewards = torch.tensor(discounted_rewards)
-        discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / (discounted_rewards.std())
-        
-        # Calculating the loss of expected rewards :
-        # grad (Expected[rewards]) = grad (policy(action|theta)* -logprob * Advantage) :
         loss = 0
-        for logprob, value, reward in zip(self.logprobs, self.state_values, discounted_rewards):
-            advantage = reward - value.item()
+        for logprob, value, reward in zip(self.logprobs, self.state_values, rewards):
+            advantage = reward  - value.item()
             action_loss = -logprob * advantage
             value_loss = F.smooth_l1_loss(value, reward)
             loss += (action_loss + value_loss)   
